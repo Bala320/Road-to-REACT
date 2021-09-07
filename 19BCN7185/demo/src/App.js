4dbb1f4d70bc8a1a5bed4da1263e3f9d5eb0f8fd
engineerscodes <practicalengineers05@gmail.com>
2021-09-07 16:06:42 +0530

    initial commit

---
+++ 19BCN7185/demo/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>HI THIS IS NAVEEN </h1>
+        </p>
+        <h4>  WELCOME TO MSC VITAP </h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
